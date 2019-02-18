2ebc396687d812460b4f3769ba00d2f0ecc21206
havared <harsha_45@hotmail.com>
2019-02-19 05:08:47 +0530

    Mongodb connection setup

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const config = require('./config');
+const { MongoManager } = require('./src/mongo');
 
 const api = require('./src/api');
 const app = express();
@@ -12,7 +13,7 @@ const cors = require("cors");
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+mongoManager.connect();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
