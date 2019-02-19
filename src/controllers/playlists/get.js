3e0782aaad9cc096e2cf68f13a03d238b6023046
havared <harsha_45@hotmail.com>
2019-02-19 19:09:41 +0530

    change in playlist get api

--- src/controllers/playlists/get.js
+++ src/controllers/playlists/get.js
@@ -1,8 +1,12 @@
-const get = ({ Playlist }, { config }) => async (req, res, next) => {
+const get = ({ Playlist, Video }, { config }) => async (req, res, next) => {
   const { _id } = req.params;
   try {
     const playlist = await Playlist.findOne({ _id });
-    if(playlist) res.status(200).send({ playlist });
+    if(playlist){
+      const video_list = await Video.find({_id: { $in: playlist.videos}})
+      if(video_list) res.status(200).send({videos: video_list});
+      else res.status(200).send({ message: 'Not found' });
+    }
     else res.status(200).send({ message: 'Not found' });
   } catch (error) {
     next(error);
